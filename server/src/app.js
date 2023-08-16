e0509436c0bdbbb9ea552348d9fee3196ff5595b
KittichokKhamkam <maggie007645@gmail.com>
2023-08-17 00:45:47 +0700

    'ch5'

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,30 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
+const { sequelize } = require('./models')
+
+const config = require('./config/config')
+
 const app = express()
 
-app.get('/status', function (req, res ){
-    res.send('Hello nodejs server')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+require('./routes')(app)
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server belong to Kittichok')
 })
 
-let port = 8081
+app.get('/hello/:name', function (req, res) {
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
+})
 
-app.listen(port, function () {
-    console.log('server running on ' + port)
+let port = process.env.PORT || config.port
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
 })
